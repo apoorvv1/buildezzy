0d08927e12eb13f51502127bfc5b3fa8e8ed0d8a
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-07-25 16:25:57 +0530

    25

--- app/Http/Controllers/JqueryController.php
+++ app/Http/Controllers/JqueryController.php
@@ -85,7 +85,7 @@ $validator=Validator::make($data,$rule,$message);
 			public function editByAjax(Request $req){
 				if($req->ajax()){
 				$i= $req->input('id');
-				Log::info("$i");
+				error_log(print_r($i));
         //$id = DB::table('learns')->where('id', $id)->get();
 				$jb = DB::table('learns')->where('id', $i)->select('id','fname','lname', 'cno','email','address')->get();
     				//return view('jquery.editByAjax',compact('jb'));
