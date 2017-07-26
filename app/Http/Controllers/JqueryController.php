01ad6108f95b78f09e5f57993ca02eed8a2613eb
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-07-26 19:56:51 +0530

    51

--- app/Http/Controllers/JqueryController.php
+++ app/Http/Controllers/JqueryController.php
@@ -89,7 +89,7 @@ DB::table('learns')->where('id', $i)->update(['fname' => $a, 'lname' => $b , 'cn
       $data=Datatables::of($learns);
       $data->addColumn('action', function ($learn) {
                 return '<button value="'.$learn->id.'" class="btn btn-primary btn-sm btn-edit">Edit</button>&nbsp;<button value="'.$learn->id.'" class="btn btn-danger btn-sm btn-dell">Delete</button>';});
-      ->editColumn('select', function($learn) {
+      $data->editColumn('select', function($learn) {
         return '<input type="checkbox" value='. $learn->id .'>';
     });          
       return $data-> make (true);
