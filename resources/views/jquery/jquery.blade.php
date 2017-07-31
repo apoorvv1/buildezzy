d021fee08f8bbd1700c395d912aeaa8a238053a0
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-08-01 03:07:22 +0530

    role3

--- resources/views/jquery/jquery.blade.php
+++ resources/views/jquery/jquery.blade.php
@@ -296,7 +296,7 @@
             {data: 'cno', name: 'cno'},
             {data: 'email', name: 'email'},
             {data: 'address', name: 'address'},
-            {data: 'role', name: 'role'},
+            {data: 'role', name: 'roles.name'},
             
             {data: 'created_at', name: 'created_at'},
             {data: 'action', name: 'action', orderable: false, searchable: false}
