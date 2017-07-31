09b4c94087453617cd421de494f31add35252bc0
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-07-31 12:34:58 +0530

    404

--- resources/views/jquery/jquery.blade.php
+++ resources/views/jquery/jquery.blade.php
@@ -173,12 +173,7 @@
 
           $(document).ready(function(){
                      
-                    $.ajaxSetup({
-                   headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                    });
-                    });
+                   
                    
                  //  $('#btn-alldell').on('click',function(e){
                    // e.preventDefault();
