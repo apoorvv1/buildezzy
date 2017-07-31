79e2d1a95248d7c78c13d0377f22f70a56f9d8bd
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-08-01 02:57:34 +0530

    role

--- app/Http/Controllers/JqueryController.php
+++ app/Http/Controllers/JqueryController.php
@@ -106,7 +106,7 @@ DB::table('learns')->where('id', $i)->update(['fname' => $a, 'lname' => $b , 'cn
 */
  
 
-			$learns = DB::table('learns')->join('roles', 'roles.id', '=', 'learns.role_id')->select('id','fname','lname', 'cno','email','address','roles.name as role','created_at');
+			$learns = DB::table('learns')->join('roles', 'roles.id', '=', 'learns.role_id')->select('id','fname','lname', 'cno','email','address','role_id','roles.name as role','created_at');
       $data=Datatables::of($learns);
       $data->addColumn('action', function ($learn) {
                 return '<button value="'.$learn->id.'" class="btn btn-primary btn-sm btn-edit">Edit</button>&nbsp;<button value="'.$learn->id.'" class="btn btn-danger btn-sm btn-dell">Delete</button>';});
