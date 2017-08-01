02a6c68c6f045915490c6e41de21fb4a8b46291b
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-08-01 12:04:30 +0530

    4

--- resources/views/jquery/jquery.blade.php
+++ resources/views/jquery/jquery.blade.php
@@ -198,8 +198,8 @@
                     
                     //var url = $(this).attr('action');
                    // var post =$(this).attr('method');
-                    var id =$(this).attr('rowid');
-                     console.log(id);
+                    var [id] =$(this).val('id');
+                     console.log([id]);
                      $.ajax({
                   type : 'post',
                   url : '{{url('alldeleteByAjax')}}',
@@ -289,7 +289,7 @@
         ],
         columns: [
             { "data": function(data){
-       return '<input type="checkbox" name="id[]" rowid="'+ data.id +'" />';
+       return '<input type="checkbox" name="id[]" value="'+ data.id +'" />';
     }, "orderable": false, "searchable":false, "name":"id" },
         
             {data: 'id', name: 'learns.id'},
