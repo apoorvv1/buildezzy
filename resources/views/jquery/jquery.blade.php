3991fc2ea864864dd023f4d7a368694a7ae35102
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-08-01 16:48:22 +0530

    ss

--- resources/views/jquery/jquery.blade.php
+++ resources/views/jquery/jquery.blade.php
@@ -203,7 +203,7 @@
                      $.ajax({
                   type : 'post',
                   url : '{{url('alldeleteByAjax')}}',
-                  data :  {data},
+                  data :  data,
                   dataType:'json',
                   success:function(data){
 
