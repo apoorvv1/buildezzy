4e268474567fde61c6c8518eb23af17ccd5b0c8b
apoorvv1 <apoorv.verma@rspinnovations.com>
2017-08-01 16:23:15 +0530

    we

--- resources/views/jquery/jquery.blade.php
+++ resources/views/jquery/jquery.blade.php
@@ -197,8 +197,9 @@
                    $(document).on('click','.btn-alldell',function(e){
                     //var data = $("#ids").val();
                     if (confirm('Are you sure you want to Delete Selected Records ?')==true) {
-                    var id=$("input[type='checkbox']").val();
-                     console.log([id]);
+                    var id;
+                    $id=$("input[type='checkbox']").val();
+                     console.log($id);
                      $.ajax({
                   type : 'post',
                   url : '{{url('alldeleteByAjax')}}',
